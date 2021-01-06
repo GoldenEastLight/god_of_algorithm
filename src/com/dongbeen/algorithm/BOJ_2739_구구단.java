1c19d84eabdb6f68ce69e63942a0b308869aaf12
goldeneastlight <goldeneastlight@gmail.com>
2021-01-06 21:12:04 +0900

    Baekjoon Online Judge 2739번 문제 구구단

---
+++ src/com/dongbeen/algorithm/BOJ_2739_구구단.java
@@ -0,0 +1,22 @@
+package com.dongbeen.algorithm;
+
+import java.util.Scanner;
+
+public class BOJ_2739_구구단 {
+	int N;
+
+	public static void main(String[] args) {
+		BOJ_2739_구구단 m = new BOJ_2739_구구단();
+		m.service();
+	}
+
+	private void service() {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(N + " * " + i + " = " + N * i);
+		}
+	}
+}
