4f28a6317c8f8e6f4b90e6ab1dd2cee00ac78296
DESKTOP-KVCQHCD\multicampus <goldeneastlight@gmail.com>
2021-02-11 16:39:59 +0900

    Baekjoon Online Judge 1927번 문제 최소힙

---
+++ src/com/dongbeen/algorithm/BOJ/BOJ_1927_최소힙.java
@@ -0,0 +1,20 @@
+package com.dongbeen.algorithm.BOJ;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class BOJ_1927_최소힙 {
+	int N;
+
+	public static void main(String[] args) {
+		BOJ_1927_최소힙 m = new BOJ_1927_최소힙();
+		m.service();
+	}
+
+	private void service() {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
+	}
+}
