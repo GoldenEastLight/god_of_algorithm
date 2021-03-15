3446c431bd5a134a4b693ce1fc024090c1f16bad
goldeneastlight <goldeneastlight@gmail.com>
2021-03-15 23:47:41 +0900

    Baekjoon Online Judge 10718번 문제 We love kriii

---
+++ src/com/dongbeen/algorithm/BOJ/BOJ_10718_WeLoveKriii.java
@@ -0,0 +1,17 @@
+package com.dongbeen.algorithm.BOJ;
+
+public class BOJ_10718_WeLoveKriii {
+	int N;
+	int M;
+
+	public static void main(String[] args) {
+		BOJ_10718_WeLoveKriii m = new BOJ_10718_WeLoveKriii();
+		m.service();
+	}
+
+	private void service() {
+		for (int i = 0; i < 2; i++) {
+			System.out.println("강한친구 대한육군");
+		}
+	}
+}
