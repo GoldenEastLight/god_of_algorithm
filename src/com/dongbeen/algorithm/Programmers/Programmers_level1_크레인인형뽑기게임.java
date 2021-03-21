38040d4ba61c82c41f8f9cf9381b9f3995cf4dfd
goldeneastlight <goldeneastlight@gmail.com>
2021-03-21 23:44:19 +0900

    no message

---
+++ src/com/dongbeen/algorithm/Programmers/Programmers_level1_크레인인형뽑기게임.java
@@ -0,0 +1,12 @@
+package com.dongbeen.algorithm.Programmers;
+
+public class Programmers_level1_크레인인형뽑기게임 {
+	public static void main(String[] args) {
+		Programmers_level1_크레인인형뽑기게임 m = new Programmers_level1_크레인인형뽑기게임();
+		m.service();
+	}
+
+	private void service() {
+		
+	}
+}
