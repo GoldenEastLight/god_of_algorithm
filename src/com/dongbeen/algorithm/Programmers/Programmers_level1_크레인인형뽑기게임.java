434fd4aba5e692a8035071c0b1eef8fd85ce5d76
goldeneastlight <goldeneastlight@gmail.com>
2021-03-23 11:40:26 +0900

    Programmers level1 크레인 인형뽑기 게임

--- src/com/dongbeen/algorithm/Programmers/Programmers_level1_크레인인형뽑기게임.java
+++ src/com/dongbeen/algorithm/Programmers/Programmers_level1_크레인인형뽑기게임.java
@@ -1,12 +1,28 @@
 package com.dongbeen.algorithm.Programmers;
 
-public class Programmers_level1_크레인인형뽑기게임 {
-	public static void main(String[] args) {
-		Programmers_level1_크레인인형뽑기게임 m = new Programmers_level1_크레인인형뽑기게임();
-		m.service();
-	}
+import java.util.Stack;
 
-	private void service() {
-		
+class Solution {
+	public int solution(int[][] board, int[] moves) {
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+
+		for (Integer move : moves) {
+			for (int i = 0; i < board.length; i++) {
+				int doll = board[i][move - 1];
+				if (doll == 0)
+					continue;
+
+				if (!stack.isEmpty() && stack.peek() == doll) {
+					stack.pop();
+					answer += 2;
+				} else {
+					stack.push(doll);
+				}
+				board[i][move - 1] = 0;
+				break;
+			}
+		}
+		return answer;
 	}
 }
