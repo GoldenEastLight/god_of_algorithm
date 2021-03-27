3c96dcab309f8099b4fed3ce82b2f7e6a9ca43f9
goldeneastlight <goldeneastlight@gmail.com>
2021-03-27 15:40:17 +0900

    Programmers level1 두 개 뽑아서 더하기

--- src/com/dongbeen/algorithm/Programmers/Programmers_level1_두개뽑아서더하기.java
+++ src/com/dongbeen/algorithm/Programmers/Programmers_level1_두개뽑아서더하기.java
@@ -1,19 +1,30 @@
 package com.dongbeen.algorithm.Programmers;
 
-public class Programmers_level1_두개뽑아서더하기 {
-	public int[] solution(int[] numbers) {
-        int[] answer = {};
+import java.util.ArrayList;
+import java.util.Collections;
 
-        for(int i = 0; i < numbers.length; i++) {
-        	combination(i, numbers);
-        }
-        
-        return answer;
-    }
-	
+public class Programmers_level1_두개뽑아서더하기 {
+	ArrayList<Integer> arr = new ArrayList<>();
+
+	public int[] solution(int[] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			combination(i, numbers);
+		}
+
+		int[] answer = new int[arr.size()];
+		Collections.sort(arr);
+
+		for (int i = 0; i < arr.size(); i++) {
+			answer[i] = arr.get(i);
+		}
+
+		return answer;
+	}
+
 	private void combination(int n, int[] numbers) {
-		for(int i = n + 1; i < numbers.length; i++) {
-			
+		for (int i = n + 1; i < numbers.length; i++) {
+			if (!arr.contains(numbers[n] + numbers[i]))
+				arr.add(numbers[n] + numbers[i]);
 		}
 	}
 }
