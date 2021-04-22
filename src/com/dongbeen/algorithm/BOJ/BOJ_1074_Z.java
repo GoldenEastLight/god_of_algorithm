3f63ba0b721887b8299f310e2436b6e5e1005bf7
goldeneastlight <goldeneastlight@gmail.com>
2021-04-22 11:30:14 +0900

    Baekjoon Online Judge 1074번 문제 Z

--- src/com/dongbeen/algorithm/BOJ/BOJ_1074_Z.java
+++ src/com/dongbeen/algorithm/BOJ/BOJ_1074_Z.java
@@ -24,49 +24,30 @@ package com.dongbeen.algorithm.BOJ;
 import java.util.Scanner;
 
 public class BOJ_1074_Z {
-	private static char map[][];
-
 	private static int N, R, C;
 	private static int answer;
-	private static boolean find;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = (int) Math.pow(2, sc.nextInt());
-
 		R = sc.nextInt();
 		C = sc.nextInt();
 
 		solve(0, 0, N);
-		System.out.println(answer);
 	}
 
-	private static void isFind(int x, int y) {
-		for (int i = x; i < x + 2; i++) {
-			for (int j = y; j < y + 2; j++) {
-				if (i == R && j == C) {
-					find = true;
-					return;
-				}
-				answer++;
-			}
-		}
-	}
-
-	private static void solve(int x, int y, int n) {
-		if (n == 2) {
-			isFind(x, y);
-			return;
+	private static void solve(int y, int x, int n) {
+		if (y == R && x == C) {
+			System.out.println(answer);
+			System.exit(0);
 		}
 
-		int max = n / 2;
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
-				if (find)
-					return;
-//            	System.out.println("호출하려는 좌표 (" + (x + max * i) + "," + (y + max * j) + ")" + max);
-				solve(x + max * i, y + max * j, max);
-			}
-		}
+		if (y <= R && R < (y + n) && x <= C && C < (x + n)) {
+			solve(y, x, n / 2);
+			solve(y, x + n / 2, n / 2); // 우측
+			solve(y + n / 2, x, n / 2); // 하단
+			solve(y + n / 2, x + n / 2, n / 2); // 대각선하단
+		} else
+			answer += n * n;
 	}
 }
