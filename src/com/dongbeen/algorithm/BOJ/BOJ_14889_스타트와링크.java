62c00ad4729788f1650db332fd83b329305946e1
goldeneastlight <goldeneastlight@gmail.com>
2021-04-26 14:38:28 +0900

    Baekjoon Online Judge 14889 스타트와 링크 문제명 오타 수정

--- src/com/dongbeen/algorithm/BOJ/BOJ_14889_스타트와링크.java
+++ src/com/dongbeen/algorithm/BOJ/BOJ_14889_스타트와링크.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_14889_스타트링크 {
+public class BOJ_14889_스타트와링크 {
 	static int N;
 	static int[][] persons;
 	static int[] selected;
