6a26eb990a0179af9fcc5412ea7b410031e97e4d
goldeneastlight <goldeneastlight@gmail.com>
2021-05-07 01:15:48 +0900

    Programmers level1 서울에서 김서방 찾기

---
+++ src/com/dongbeen/algorithm/Programmers/Programmers_level1_서울에서김서방찾기.java
@@ -0,0 +1,11 @@
+package com.dongbeen.algorithm.Programmers;
+
+public class Programmers_level1_서울에서김서방찾기 {
+	public String solution(String[] seoul) {
+		String answer = "";
+		for (int i = 0; i < seoul.length; i++)
+			if (seoul[i].equals("Kim"))
+				return answer = "김서방은 " + i + "에 있다";
+		return answer;
+	}
+}
