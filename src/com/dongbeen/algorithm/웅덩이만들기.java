93e8444c63a935edb78666b8513b63b5f7bcc742
goldeneastlight <goldeneastlight@gmail.com>
2021-05-29 17:04:37 +0900

    Programmers 웅덩이 만들기

---
+++ src/com/dongbeen/algorithm/웅덩이만들기.java
@@ -0,0 +1,41 @@
+package com.dongbeen.algorithm;
+
+public class 웅덩이만들기 {
+	public static void main(String[] args) {
+		int[] bricks = { 1, 2, 5, 3, 1, 0, 2, 3 };
+		int n = 6;
+		int k = 3;
+		// result == 5;
+
+		웅덩이만들기 m = new 웅덩이만들기();
+		System.out.println(m.solution(bricks, n, k));
+	}
+
+	public int solution(int[] bricks, int n, int k) {
+		int answer = 0;
+
+		for (int i = 0; i < k - 1; i++) {
+			answer = buildBricks(bricks, n, answer);
+		}
+
+		return answer;
+	}
+
+	public int buildBricks(int[] bricks, int n, int answer) {
+		int blanc = 0;
+		int blancIdx = 0;
+
+		for (int i = 1; i < bricks.length - 1; i++) {
+			if (bricks[i] != n && bricks[i - 1] < n && bricks[i + 1] < n) {
+				if (blanc < bricks[i]) {
+					blanc = bricks[i];
+					blancIdx = i;
+				}
+			}
+		}
+		answer += n - blanc;
+		bricks[blancIdx] += n - blanc;
+
+		return answer;
+	}
+}
